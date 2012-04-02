b924f82c50b3ae623108630ee3968c4e94b6f7e9
Brendan Baldwin <brendan@usergenic.com>
2012-04-02 10:29:52 -0700

    Logger GGRRR

--- lib/toad.rb
+++ lib/toad.rb
@@ -9,11 +9,13 @@ module Toad
     @env ||= ENV["TOAD_ENV"] || ENV["RACK_ENV"] || "development"
   end
 
-  Toad::Logger = Object::Logger.new(ENV["TOAD_LOG"] || $stdout) unless ENV["TOAD_NOLOG"]
+  unless Toad.constants.any? { |c| c.to_s == "Logger" }
+    Toad::Logger = Object::Logger.new(ENV["TOAD_LOG"] || $stdout) unless ENV["TOAD_NOLOG"]
 
-  original_formatter = Object::Logger::Formatter.new
-  Logger.formatter = proc do |severity, datetime, progname, msg|
-    original_formatter.call(severity, datetime, progname, caller[4].to_s + " => " + msg.inspect)
+    original_formatter = Object::Logger::Formatter.new
+    Logger.formatter = proc do |severity, datetime, progname, msg|
+      original_formatter.call(severity, datetime, progname, caller[4].to_s + " => " + msg.inspect)
+    end
   end
 
   Models.connect!
