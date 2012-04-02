c5a78b35c21914ea937242461320d70885916c7f
Brendan Baldwin <brendan@usergenic.com>
2012-04-02 10:05:41 -0700

    Logger setup to help figure out WTF on heroku.

--- lib/toad.rb
+++ lib/toad.rb
@@ -11,5 +11,10 @@ module Toad
 
   Logger = Object::Logger.new(ENV["TOAD_LOG"] || $stdout) unless ENV["TOAD_NOLOG"]
 
+  original_formatter = Object::Logger::Formatter.new
+  Logger.formatter = proc do |severity, datetime, progname, msg|
+    original_formatter.call(severity, datetime, progname, caller[4].to_s + " => " + msg.inspect)
+  end
+
   Models.connect!
 end
